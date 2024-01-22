5d181b39ba8f683661558c949b1d7e8ed7789c0d
El Mehdi Kenane <elmehdi.kenane@gmail.com>
2024-01-22 01:00:47 +0100

    call process method instead of true loop

--- src/main.cpp
+++ src/main.cpp
@@ -10,28 +10,8 @@ int main(int argc, const char *argv[])
 		return (EXIT_FAILURE);
 	Server::setPassword(argv[1]);
 	Server::setPort(std::atoi(argv[2]));
-
-	setupServerSocket();
-
-	Client newClient;
-	while (1)
-	{
-		if (!Server::ReceiveRequest(message))
-			break;
-		std::cout << "========== NEW MSG ==========" << std::endl;
-		std::cout << message << std::endl;
-		commands = ft_split(message, "\r\n");
-		Server::parseCommands(commands, newClient);
-		if (Server::getPassword() != newClient.getPassword() && newClient.getNickName() != "")
-		{
-			message = ": 464 " + newClient.getNickName() + " :Password incorrect\r\n";
-			Server::responseMsg(message);
-		}
-		else if (newClient.getNickName() != "" && newClient.getLoginName() != "" && newClient.getRealName() != "")
-			Server::addClient(newClient);
-	}
-
-	cleanupResources();
-
+	Server::setupServerSocket();
+	Server::process();
+	Server::cleanupResources();
 	return (EXIT_SUCCESS);
 }
